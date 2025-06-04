0a42dc94a29ada3a7a3123a2de904a3313f863be
R-DiEusanio <dieusanioriccardo@gmail.com>
2025-06-04 10:26:25 +0200

    aggiunta dependency OpenFeign

--- src/main/java/com/example/corso/CorsoApplication.java
+++ src/main/java/com/example/corso/CorsoApplication.java
@@ -2,8 +2,10 @@ package com.example.corso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CorsoApplication {
 
 	public static void main(String[] args) {
